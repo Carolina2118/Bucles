bd7115068b48432d71c8add4fe68261bfe2c0a19
Carolina2118 <loboscarolinaa@gmail.com>
2022-09-01 19:06:48 -0400

    for bucles

---
+++ bucles.js
@@ -0,0 +1,6 @@
+// for El bucle típico incluye una variable iteradora que suele aparecer en las tres expresiones
+//Un for bucle contiene tres expresiones separadas;entre paréntesis:
+
+for(let numeros =0;numeros < 5; numeros ++ ){
+    console.log(numeros);
+}
