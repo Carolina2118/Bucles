5d6284718eec1d23fb47fa43142ef56890dfefb0
Carolina2118 <loboscarolinaa@gmail.com>
2022-09-07 17:47:16 -0400

    bucles, while y Do...while

--- buclesWhile.js
+++ buclesWhile.js
@@ -1,5 +1,28 @@
-let conteo = 1; 
+// La sintaxis de un whilebucle es ideal cuando no sabemos de antemano cuántas veces debe ejecutarse el bucle.
+
+let conteoUno = 1; 
 while (conteoUno < 5){
     console.log(conteoUno);
-    conteo ++;    
+    conteoUno ++;    
 }
+
+// Declaracion Do ...while .
+
+let DoWhile = '';
+let i= 0;
+ do{
+    DoWhile = DoWhile +i;
+    i ++;
+ } while (i < 5);
+  console.log(DoWhile);
+  //Somos golosos, por lo que queremos agregar al menos una taza de azúcar a la masa, 
+  //incluso si el valor de cupsOfSugarNeededes 0.
+  //Cree un do...whilebucle que se incremente cupsAddeden uno mientras cupsAddedsea menor que cupsOfSugarNeeded.
+  
+let cupsOfSugarNeeded = 5;
+let  cupsAdded = 0;
+   do{
+     cupsAdded ++;
+   console.log(cupsAdded)
+   } while (cupsAdded<cupsOfSugarNeeded);
+   
