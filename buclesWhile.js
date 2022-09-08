b9fb983e0524558a65254749d95ef379f5f0c4d2
Carolina2118 <loboscarolinaa@gmail.com>
2022-09-08 19:19:09 -0400

    bucles,ruptura break

--- buclesWhile.js
+++ buclesWhile.js
@@ -17,7 +17,7 @@ let i= 0;
   console.log(DoWhile);
   //Somos golosos, por lo que queremos agregar al menos una taza de azúcar a la masa, 
   //incluso si el valor de cupsOfSugarNeededes 0.
-  //Cree un do...whilebucle que se incremente cupsAddeden uno mientras cupsAddedsea menor que cupsOfSugarNeeded.
+  //Cree un do...while bucle que se incremente cupsAddeden uno mientras cupsAddedsea menor que cupsOfSugarNeeded.
   
 let cupsOfSugarNeeded = 5;
 let  cupsAdded = 0;
@@ -25,4 +25,26 @@ let  cupsAdded = 0;
      cupsAdded ++;
    console.log(cupsAdded)
    } while (cupsAdded<cupsOfSugarNeeded);
-   
+
+// La palabra clave de ruptura es break.
+
+
+for (let i = 0; i < 99; i++) {
+  if (i > 2 ) {
+     break;
+  }
+  console.log('Banana.');
+}
+ 
+console.log('Naranja, te alegra que haya roto el bucle');
+
+const rappero = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rappero.length; i++){
+  console.log(rappero[i]);
+  if ( rappero[i] === 'Notorious B.I.G.'){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.");
+
