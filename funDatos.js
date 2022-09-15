146fb92fec36c77e34af4905c3b1f91769d94455
Carolina2118 <loboscarolinaa@gmail.com>
2022-09-15 19:16:59 -0300

    bucles, funciones  como datos

---
+++ funDatos.js
@@ -0,0 +1,10 @@
+// comprueba que dos más dos son iguales a cuatro millones de veces.
+
+const funcionDatos = ()=>{
+
+    for ( let i = 1; i <=1000000; i ++){
+        if( 2+2 != 4){
+        console.log("algo esta mal  :( ");
+    }
+}
+};
