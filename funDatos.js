af7542627651e27c47c3c1b11bb703a359da4835
Carolina2118 <loboscarolinaa@gmail.com>
2022-09-20 19:17:10 -0300

    bucles,funciones como parámetros

--- funDatos.js
+++ funDatos.js
@@ -4,7 +4,21 @@ const funcionDatos = ()=>{
 
     for ( let i = 1; i <=1000000; i ++){
         if( 2+2 != 4){
-        console.log("algo esta mal  :( ");
+            console.log("algo esta mal:( ");
+        }
     }
-}
 };
+// escribiendo funciones de orden superior .
+
+
+const sumaDos = num => {
+    return num + 2;
+  }
+
+  const funcionOrden = (func, val) =>{
+    let A = val + 2;
+    let B = func(val);
+    return A === B ? func(val) : "resultado";
+
+  };
+  console.log(funcionOrden(sumaDos, 10));
