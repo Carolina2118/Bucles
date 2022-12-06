8e38808242df98301abb3cdbad026af5077df6d6
Carolina2118 <loboscarolinaa@gmail.com>
2022-12-06 20:00:01 -0300

    practicando get

--- objetosAvanzados.js
+++ objetosAvanzados.js
@@ -24,6 +24,19 @@ const robot = {
    
   goat.diet(); // Prints undefined
 
+  // metodo Getters
   
+ const auto = {
+  _model:'Escarabajo',
+  _marca:'Volkswagen',
+  get autoFavorito() {
+    if (this._marca && this._model){
+      return `Mi marca favorita de auto es:${this._marca} y modelo: ${this._model}`;
+    }else{
+      return "No es mi auto favorito";
+    }
+    
+  }
   
- 
+};
+console.log(auto.autoFavorito);
